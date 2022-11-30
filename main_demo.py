0e55bc6b5dc7c6e3f14309af274cb0168128b25f
Elie Lévy <elie.levy.3@gmail.com>
2022-11-30 15:15:46 +0100

    updating title

--- main_demo.py
+++ main_demo.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 
+st.set_page_config(layout="wide", page_title="Airbnb Bookings Exploration", page_icon="🏠", menu_items={"Get Help" : "https://github.com/elielevy3/STREAMLIT_DEPLOYMENT_SHOWCASE"})
+st.markdown("# Welcome to London AirBnb Bookings Exploration 🏠")
+
 @st.experimental_singleton
 def get_data(sample_size): 
 
@@ -16,31 +19,27 @@ def get_data(sample_size):
     # sample
     return data.sample(sample_size)
 
-st.set_page_config(layout="wide", page_title="Airbnb Bookings Exploration", page_icon="🏠", menu_items={"Get Help" : "https://github.com/elielevy3/STREAMLIT_DEPLOYMENT_SHOWCASE"})
-st.markdown("# Welcome to London AirBnb")
-
-# get data
 data = get_data(3000)
 
-col_1, col_2, col_3, col4 = st.columns(4)
+col1, col2, col3, col4 = st.columns(4)
 
-# let the user pick the column he wants to display
-with col_1: 
+with col1: 
     st.header("Column selection")
     selected_cols = st.multiselect("Choose the column you want to display", data.columns)
     if st.checkbox("All columns"): 
         selected_cols = data.columns
 
-with col_2: 
-    st.header("Neighbourhood")
-    selected_hoods = st.multiselect('Which neighbourhood do you want to pick ? ', data["neighbourhood"].unique())
-    # handling the case in which the user does not care about the neighbourhood
-    if st.checkbox("All Neighbourhoods"): 
+with col2: 
+    st.header("Hoods selection")
+    selected_hoods = st.multiselect("Choose the hoods you want", data["neighbourhood"].unique())
+    if st.checkbox("All hoods"): 
         selected_hoods = data["neighbourhood"].unique()
+    selected_hoods
 
-with col_3:
+with col3:
     st.header("Max price")
     min_price, max_price = st.slider("Pick your min and max price", value=(0, 1000))
+
 
 with col4: 
     st.header("Room type")
@@ -48,11 +47,13 @@ with col4:
     room_types_dict = {room_type: st.checkbox(room_type) for room_type in data["room_type"].unique()}
     selected_room_types = [room_type for room_type, selected in room_types_dict.items() if selected]
 
+
 # display dataframe and plot
 results = data[(data.price <= max_price) & (data.price >= min_price) & (data.neighbourhood.isin(selected_hoods)) & (data.room_type.isin(selected_room_types))]
 st.session_state.results = results
 
-col1, col2 = st.columns(2)
+
+col1, col2 = st.columns([1, 2])
 
 with col1:  
     if st.sidebar.checkbox("Display dataframe"):   
@@ -63,7 +64,6 @@ with col2:
     if st.sidebar.checkbox("Display map"):
         st.map(results)
 
-# check the code
 if st.checkbox("Display code of the page"):
     with open("./Main_Page.py", 'r') as file:
         st.code(file.read(), language='python')
